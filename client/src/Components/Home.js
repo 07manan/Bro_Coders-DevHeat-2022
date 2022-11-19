118ed461f503b9570ca02415a66329ff6896b19f
NAVDEEP MAHESHWARI <77584053+Navdeepmaheshwari@users.noreply.github.com>
2022-11-19 10:12:34 +0530

    home add

---
+++ client/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <div>
+      Home
+      <div>
+        <a href="/login">Login</a>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
