fe9e2227e79ff393034862802de1a48b15fc3452
07manan <07manan1850@gmail.com>
2022-11-19 16:51:15 +0530

    login update

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "../Components/register.css";
 
 const LoginForm = () => {
+  const [userType, setUserType] = useState("teacher");
   const [user, setUser] = useState({
     username: "",
     password: "",
@@ -16,53 +17,143 @@ const LoginForm = () => {
     setUser({ ...user, [name]: value });
   };
   const handleLogin = async (e) => {};
+  const handleSubmit = async (e) => {};
   return (
     <div className="registration-form">
-      <form>
-        <div className="form-icon">
-          <span>
-            <i className="icon icon-user"></i>
-          </span>
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control item"
-            onChange={handleInputs}
-            id="username"
-            name="username"
-            value-={user.username}
-            placeholder="Username"
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="password"
-            className="form-control item"
-            onChange={handleInputs}
-            id="password"
-            name="password"
-            value={user.password}
-            placeholder="Password"
-          />
-        </div>
-
-        <div className="form-group">
+      {userType == "teacher" ? (
+        <form>
           <button
-            type="button"
-            className="btn btn-block create-account"
-            onClick={handleLogin}
+            position="left"
+            style={{
+              marginLeft: "-50px",
+              backgroundColor: "white",
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            className="tab-button"
           >
-            Login
+            Teacher
           </button>
-        </div>
-        <div>
-          <span className="center">
-            Don't have an account? <a href="/register">Create Account</a>
-          </span>
-        </div>
-      </form>
+          <button
+            position="right"
+            style={{ marginRight: "-50px" }}
+            className="tab-button"
+            onClick={(e) => {
+              e.preventDefault();
+              setUserType("student");
+            }}
+          >
+            Student
+          </button>
+          <div className="form-icon">
+            <span>
+              <i className="icon icon-user"></i>
+            </span>
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              onChange={handleInputs}
+              id="username"
+              name="username"
+              value-={user.username}
+              placeholder="Username"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control item"
+              onChange={handleInputs}
+              id="password"
+              name="password"
+              value={user.password}
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="form-group">
+            <button
+              type="button"
+              className="btn btn-block create-account"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+          <div>
+            <span className="center">
+              Don't have an account? <a href="/register">Create Account</a>
+            </span>
+          </div>
+        </form>
+      ) : (
+        <form>
+          <button
+            position="left"
+            style={{ marginLeft: "-50px" }}
+            className="tab-button"
+            onClick={(e) => {
+              e.preventDefault();
+              setUserType("teacher");
+            }}
+          >
+            Teacher
+          </button>
+          <button
+            position="right"
+            style={{ marginRight: "-50px", backgroundColor: "white" }}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            className="tab-button"
+          >
+            Student
+          </button>
+          <div className="form-icon">
+            <span>
+              <i className="icon icon-user"></i>
+            </span>
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              onChange={handleInputs}
+              id="username"
+              name="username"
+              value-={user.username}
+              placeholder="Username"
+            />
+          </div>
+
+          <select
+            className="form-control item"
+            style={{ height: "auto" }}
+            name="class"
+            id="class"
+          >
+            <option value={null}>Select Batch</option>
+            <option value="ECE 5th sem">ECE 5th sem</option>
+            <option value="ECE 3rd sem">ECE 3rd sem</option>
+            <option value="CSE 3rd sem">CSE 3rd sem</option>
+            <option value="CSE 5th sem">CSE 5th sem</option>
+          </select>
+
+          <div className="form-group">
+            <button
+              type="button"
+              className="btn btn-block create-account"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
       <div className="social-media">
         {/* <h5>Sign up with social media</h5> */}
         <div className="social-icons">
